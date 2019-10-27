f6b1167c0d802f1f8e9ce51b4cb8c4534d8f0796
devPinheiro <samuelpinheiro41@gmail.com>
2019-10-27 22:41:47 +0100

    show model

--- fyurr/app.py
+++ fyurr/app.py
@@ -515,6 +515,12 @@ def create_shows():
 def create_show_submission():
   # called to create new shows in the db, upon submitting new show listing form
   # TODO: insert form data as a new Show record in the db, instead
+  venue_id = request.form['venue_id']
+  artist_id = request.form['artist_id']
+  shows = show_item(artist_id=artist_id, venue_id=venue_id)
+  db.session.add(shows)
+  db.session.commit()
+
 
   # on successful db insert, flash success
   flash('Show was successfully listed!')
